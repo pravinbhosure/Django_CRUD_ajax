cdfa94f9e764c45134c2f553fb7252f2395b65f4
pravinbhosure <name@domain.example>
2024-04-09 13:18:37 +0530

    first commit

---
+++ enroll/models.py
@@ -0,0 +1,12 @@
+import email
+from sre_constants import MAX_UNTIL
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=70)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+     
+
